574a639d1533c3f56589ae87628d199d2bbfbbdd
federicopeker <federicopeker@gmail.com>
2014-05-05 15:22:39 -0300

    SegManager Implementacion de SEGMNGR-1

--- seg_mngr/models.py
+++ seg_mngr/models.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class ConfigIp(models.Model):
+	"""
+	Una configuracion Ip del servidor
+	"""
+	ip_address = models.CharField(max_length=64)
+
+ 	def __unicode__(self):
+ 		return self.ip_address
+
+class Server(models.Model):
+	"""
+	Clase sevidor
+	"""
+	name = models.CharField(max_length=64)
+	address_ip = models.ForeignKey('ConfigIp')
+	operating_system = models.CharField(max_length=64)
+	location = models.CharField(max_length=64)
+
+	def __unicode__(self):
+ 		return self.name
+
