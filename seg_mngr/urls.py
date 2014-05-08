235105e094bec24e69e98133d28bd3f70375b8ae
federicopeker <federicopeker@gmail.com>
2014-05-08 17:26:13 -0300

    Revert "SegManager Implementacion SEGMNGR-11"
    This reverts commit 13c072f360646e249a589930c383fd89af205417.

--- seg_mngr/urls.py
+++ seg_mngr/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^$', 'seg_mngr.views.home', name='home'),
-    url(r'^server_manager/', 'seg_mngr.views.server_manager', name='server_manager_view'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
--- seg_mngr/views.py
+++ seg_mngr/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render_to_response
 
 def home(request):
     return render_to_response("seg_mngr/index.html")
-
-def server_manager(request):
-    return render_to_response("seg_mngr/server_manager.html")
