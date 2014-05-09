ac0d725e8db68819881ca71fe6e9532dd1148dff
federicopeker <federicopeker@gmail.com>
2014-05-09 14:34:08 -0300

    SegManager Implementacion SEGMNGR-11 Migracion 0004

---
+++ seg_mngr/migrations/0004_auto__add_servertask.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ServerTask'
+        db.create_table(u'seg_mngr_servertask', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('server', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['seg_mngr.Server'])),
+            ('task', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['seg_mngr.Task'])),
+            ('state', self.gf('django.db.models.fields.CharField')(max_length=64)),
+        ))
+        db.send_create_signal(u'seg_mngr', ['ServerTask'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ServerTask'
+        db.delete_table(u'seg_mngr_servertask')
+
+
+    models = {
+        u'seg_mngr.configip': {
+            'Meta': {'object_name': 'ConfigIp'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'server': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['seg_mngr.Server']"})
+        },
+        u'seg_mngr.server': {
+            'Meta': {'object_name': 'Server'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'operating_system': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'task': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['seg_mngr.Task']", 'symmetrical': 'False', 'through': u"orm['seg_mngr.ServerTask']", 'blank': 'True'})
+        },
+        u'seg_mngr.servertask': {
+            'Meta': {'object_name': 'ServerTask'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'server': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['seg_mngr.Server']"}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['seg_mngr.Task']"})
+        },
+        u'seg_mngr.task': {
+            'Meta': {'object_name': 'Task'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        }
+    }
+
+    complete_apps = ['seg_mngr']
