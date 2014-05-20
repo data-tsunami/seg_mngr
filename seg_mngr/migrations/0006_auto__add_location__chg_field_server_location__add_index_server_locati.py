634174179a3421159f051ebe9514273812bf84a2
federicopeker <federicopeker@gmail.com>
2014-05-20 16:22:13 -0300

    SegManager Implementacion SEGMNGR-14 Migración

---
+++ seg_mngr/migrations/0006_auto__add_location__chg_field_server_location__add_index_server_locati.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Location'
+        db.create_table(u'seg_mngr_location', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=64)),
+        ))
+        db.send_create_signal(u'seg_mngr', ['Location'])
+
+
+        # Renaming column for 'Server.location' to match new field type.
+        db.rename_column(u'seg_mngr_server', 'location', 'location_id')
+        # Changing field 'Server.location'
+        db.alter_column(u'seg_mngr_server', 'location_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['seg_mngr.Location']))
+        # Adding index on 'Server', fields ['location']
+        db.create_index(u'seg_mngr_server', ['location_id'])
+
+
+    def backwards(self, orm):
+        # Removing index on 'Server', fields ['location']
+        db.delete_index(u'seg_mngr_server', ['location_id'])
+
+        # Deleting model 'Location'
+        db.delete_table(u'seg_mngr_location')
+
+
+        # Renaming column for 'Server.location' to match new field type.
+        db.rename_column(u'seg_mngr_server', 'location_id', 'location')
+        # Changing field 'Server.location'
+        db.alter_column(u'seg_mngr_server', 'location', self.gf('django.db.models.fields.CharField')(max_length=64))
+
+    models = {
+        u'seg_mngr.configip': {
+            'Meta': {'object_name': 'ConfigIp'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'server': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['seg_mngr.Server']"})
+        },
+        u'seg_mngr.location': {
+            'Meta': {'object_name': 'Location'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'seg_mngr.operatingsystem': {
+            'Meta': {'object_name': 'OperatingSystem'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'seg_mngr.server': {
+            'Meta': {'object_name': 'Server'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['seg_mngr.Location']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'operating_system': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['seg_mngr.OperatingSystem']"}),
+            'task': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['seg_mngr.Task']", 'symmetrical': 'False', 'through': u"orm['seg_mngr.ServerTask']", 'blank': 'True'})
+        },
+        u'seg_mngr.servertask': {
+            'Meta': {'object_name': 'ServerTask'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'server': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['seg_mngr.Server']"}),
+            'state': ('django.db.models.fields.CharField', [], {'default': "'P'", 'max_length': '64'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['seg_mngr.Task']"})
+        },
+        u'seg_mngr.task': {
+            'Meta': {'object_name': 'Task'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        }
+    }
+
+    complete_apps = ['seg_mngr']
