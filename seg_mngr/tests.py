1f4c37b43ab63de62aa26a0cf760e37da2200b3d
Horacio G. de Oro <hgdeoro@gmail.com>
2014-05-28 12:35:32 -0300

    Agrega esqueleto de tests

--- seg_mngr/tests.py
+++ seg_mngr/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestMatrixGenerator(TestCase):
+
+    def test_matrix_is_generated(self):
+        """Tests that a matrix is generated successfully"""
+
+        # TODO: crear servidores server_1, server_2, server_3
+
+        # TODO: crear tasks task_1, task_2, task_3
+
+        # TODO: cambiar estados:
+        #  s1 t1 -> NO_APLICA
+        #  s2 t3 -> EN_CURSO
+        #  s1 t2 -> FINALIZADA
+
+        # -----
+
+        # TODO: generar matrix
+
+        # self.assertXxxx() de que la matrix contiene los datos correctos
