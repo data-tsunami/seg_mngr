b8597ad835478703f0e248a6f0eff417c7eb0eb8
federicopeker <federicopeker@gmail.com>
2014-06-17 15:35:36 -0300

    SegManager Implementacion SEGMNGR-23

--- seg_mngr/models.py
+++ seg_mngr/models.py
@@ -73,6 +73,9 @@ class Server(models.Model):
 
     def getIp(self):
         return ConfigIp.objects.filter(server=self)
+    
+    def get_cross_check(self):
+        return CrossCheck.objects.filter(server=self).order_by('check_date')
 
 
 class ConfigIp(models.Model):
